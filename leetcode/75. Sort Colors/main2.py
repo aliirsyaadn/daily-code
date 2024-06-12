ae6589fc2b98d47e9426ae8f816ec8e3b1844c28
Ali Irsyaad Nursyaban <aliirsyaadnursyaban@gmail.com>
2024-06-12 13:37:20 +0700

    feat(leetcode): solved 75. Sort Colors

---
+++ leetcode/75. Sort Colors/main2.py
@@ -0,0 +1,57 @@
+# https://leetcode.com/problems/sort-colors/description/
+# 75. Sort Colors
+# Medium
+# Topics
+# Companies
+# Hint
+# Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+# You must solve this problem without using the library's sort function.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
+# Example 2:
+
+# Input: nums = [2,0,1]
+# Output: [0,1,2]
+ 
+
+# Constraints:
+
+# n == nums.length
+# 1 <= n <= 300
+# nums[i] is either 0, 1, or 2.
+ 
+
+# Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+def sortColors(nums: list[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    quickSort(nums, 0, len(nums) - 1)
+    return nums
+
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+ 
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+ 
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+ 
+    return i + 1
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
